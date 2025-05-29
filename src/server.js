682e686a84b747f7b431584e4d828eff12111f60
Jordan Asano <102700387+jordanasano@users.noreply.github.com>
2025-05-29 15:12:22 -0700

    Delete src/server.js

--- src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-exports.app = require("./app");
-const PORT = 3001;
-exports.app.listen(PORT, function () {
-    console.log(`Started on http://localhost:${PORT}`);
-});
-//# sourceMappingURL=server.js.map
